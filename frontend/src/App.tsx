e0d1b1e8989377ffff2c42212572459f396d31b1
Rida Akhsas <rakhsas@e1r3p11.1337.ma>
2024-01-16 12:11:07 +0100

    handling volume mounting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+// import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
 function App() {
   return (
-    <div className="App">
-      <div className="bg-gray-200 h-screen flex items-center justify-center">
-        <div className="bg-white p-8 rounded shadow-md">
-          <h1 className="text-3xl font-bold mb-4">Tailwind CSS with React</h1>
-          <p className="text-gray-600">
-            This is a simple example of using Tailwind CSS with React.
-          </p>
-        </div>
+    <div className="bg-gray-200 h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h1 className="text-3xl font-bold mb-4">Tailwind CSS with React</h1>
+        <img
+          className="w-full mb-4 rounded"
+          src={logo}
+          alt="Sample Image"
+        />
+        <p className="text-gray-600">
+          This is a simple example of using Tailwind CSS with React.
+        </p>
       </div>
     </div>
   );
