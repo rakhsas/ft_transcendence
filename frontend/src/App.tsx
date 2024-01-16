3b80626a5378291f36dae6156efd6fe904f24406
Rida Akhsas <rakhsas@e1r3p11.1337.ma>
2024-01-16 11:06:11 +0100

    tailwind added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,22 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-gray-200 h-screen flex items-center justify-center">
+        <div className="bg-white p-8 rounded shadow-md">
+          <h1 className="text-3xl font-bold mb-4">Tailwind CSS with React</h1>
+          <p className="text-gray-600">
+            This is a simple example of using Tailwind CSS with React.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
