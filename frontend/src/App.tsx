1a418de018286b22e405af2d2357f441bba65010
Rida Akhsas <rakhsas@e1r3p11.1337.ma>
2024-01-16 15:28:48 +0100

    change docker compose bind mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 import './App.css';
 function App() {
@@ -7,11 +7,11 @@ function App() {
     <div className="bg-gray-200 h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-md">
         <h1 className="text-3xl font-bold mb-4">Tailwind CSS with React</h1>
-        <img
+        {/* <img
           className="w-full mb-4 rounded"
           src={logo}
           alt="Sample Image"
-        />
+        /> */}
         <p className="text-gray-600">
           This is a simple example of using Tailwind CSS with React.
         </p>
