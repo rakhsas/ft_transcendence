123e4d19289b24be71e9a0de79f5ced65abd83c5
Rida Akhsas <rakhsas@e1r3p11.1337.ma>
2024-01-31 20:29:04 +0100

    update the sideBar disposition

--- frontend/src/Components/dashboard/Dashboard.tsx
+++ frontend/src/Components/dashboard/Dashboard.tsx
@@ -1,13 +1,17 @@
+import { Engine } from "@tsparticles/engine";
 import SidebarComponent from "../shared/sidebar/sidebar";
 import "./Dashboard.css"
+import Particle from "../shared/particles/particleBg";
 function DashboardComponent(): JSX.Element {
-    return (
-        <>
-            <div className="max-w-screen-2xl lg:max-w-screen-xl md:max-w-screen-md sm:max-w-screen-sm all">
-                <SidebarComponent/>
-            </div>
-        </>
-    )
+  
+  return (
+    <>
+      <div className="max-w-screen-2xl lg:max-w-screen-xl md:max-w-screen-md sm:max-w-screen-sm all">
+        <Particle />
+        <SidebarComponent />
+      </div>
+    </>
+  )
 }
 
 export default DashboardComponent;
